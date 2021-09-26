3d1e2b6c4ec4dbd7e154fce57b365902e6d92e2f
Muhammad Subhan <msd3vil@gmail.com>
2021-09-26 16:03:10 +0500

    a simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function decrement(){
+    setCounter(counter - 1);
+  }
+  function Increment(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <>
+    <h1>Counter: {counter}</h1>
+    <button onClick={decrement}>decrement</button>
+    <button onClick={Increment}>Increment</button>
+    </>
   );
 }
 
